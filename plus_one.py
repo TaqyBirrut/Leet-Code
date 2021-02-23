a46398c350c62ecb8eb85306cebae42a4655bd51
TaqyBirrut <47145415+TaqyBirrut@users.noreply.github.com>
2021-02-23 13:20:09 +0700

    Update plus_one.py

--- plus_one.py
+++ plus_one.py
@@ -1,10 +1,16 @@
 def TambahAkhir (digits):
     hasil = digits[-1]+1
-    digits.pop()
-    digits.append(hasil)
-    return digits
+    if hasil >= 10:
+        hasil = list(map(int, str(hasil)))
+        digits.pop()
+        digits.extend(hasil)
+        return digits
+    else:
+        digits.pop()
+        digits.append(hasil)
+        return digits
 
 def main():
-    digits = [1,2,3]
+    digits = [9,9]
     print(TambahAkhir(digits))
 main()
